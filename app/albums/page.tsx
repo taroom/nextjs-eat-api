889a685b319883f08ac47876c029a1fba8163cd6
Sultan Room <sultanroomcode@gmail.com>
2024-06-21 22:24:48 +0700

    add albums page

---
+++ app/albums/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Albums() {
+  return (
+    <>
+      <h1>Halaman Album</h1>
+
+      <Link href="/">Kembali</Link><br />
+    </>
+  )
+}
+
+export default Albums
